a314871a13a1bbca1d9a2499683a99f1858fbab4
moshek <moshe.kerbel@kenshoo.com>
2017-06-29 07:01:03 +0300

    first commit

---
+++ service-worker.js
@@ -0,0 +1,14 @@
+self.addEventListener('fetch', event => {
+  debugger;
+  var request = event.request;
+  if (request.mode === 'navigate' ||
+    (request.method === 'GET' &&
+     request.headers.get('accept').includes('text/html'))) {
+    event.respondWith(
+      fetch(request).catch(error => {
+        return new Response('<p>Oh, dear.</p>',
+          { headers: { 'Content-Type': 'text/html' } });
+      })
+    );
+  }
+});
